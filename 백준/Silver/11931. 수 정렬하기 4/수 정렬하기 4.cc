1f0227ede8d4f185e21859833d250b7d2ef4d1d6
정수현 <jsh00325@naver.com>
2023-07-16 21:18:13 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 268 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -1,33 +1,14 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#define FOR(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define FOR(i, n) for(int (i) = 0; (i) < (n); ++i)
 
-const int MX = 1000000;
-int arr[MX], temp[MX];
-
-void merge(int s, int e) {
-	int mid = (s + e) / 2;
-	int i = s, j = mid + 1, k = s;
-	while (i <= mid && j <= e)
-		if (arr[i] > arr[j]) temp[k++] = arr[i++];
-		else temp[k++] = arr[j++];
-	while (i <= mid) temp[k++] = arr[i++];
-	while (j <= e) temp[k++] = arr[j++];
-	for (int x = s; x <= e; x++) arr[x] = temp[x];
-}
-void merge_sort(int s, int e) {
-	if (s == e) return;
-	int mid = (s + e) / 2;
-	merge_sort(s, mid);
-	merge_sort(mid + 1, e);
-	merge(s, e);
-}
+int n, arr[1000000];
 
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0);
+    ios::sync_with_stdio(0); cin.tie(0);
 
-	int n; cin >> n;
-	FOR(i, n) cin >> arr[i];
-	merge_sort(0, n - 1);
-	FOR(i, n) cout << arr[i] << '\n';
+    cin >> n;
+    FOR(i, n) cin >> arr[i];
+    sort(arr, arr+n, [](const int& a, const int& b){return a > b;});
+    FOR(i, n) cout << arr[i] << '\n';
 }
