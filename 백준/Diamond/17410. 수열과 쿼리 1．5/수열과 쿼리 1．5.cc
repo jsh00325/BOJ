e629cbae4f16d6c6a9a0962cfd7c6f1ccd64922f
정수현 <jsh00325@naver.com>
2023-08-22 01:17:14 +0900

    [Diamond V] Title: 수열과 쿼리 1.5, Time: 1104 ms, Memory: 2820 KB -BaekjoonHub

--- 백준/Diamond/17410. 수열과 쿼리 1．5/수열과 쿼리 1．5.cc
+++ 백준/Diamond/17410. 수열과 쿼리 1．5/수열과 쿼리 1．5.cc
@@ -3,9 +3,9 @@ using namespace std;
 #define FOR(i, n) for(int (i) = 0; (i) < (n); ++i)
 #define FOR1(i, n) for(int (i) = 1; (i) <= (n); ++i)
 
-const int m = 1000;
+const int m = 650;
 int n, arr[100001];
-struct Bucket { int arr[m], size; } buc[101];
+struct Bucket { int arr[m], size; } buc[160];
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
