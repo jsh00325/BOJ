df7f167c6822258f24308c8ec431951d21d17c7a
정수현 <jsh00325@naver.com>
2024-02-12 00:11:15 +0900

    [Gold III] Title: 짠돌이 호석, Time: 12 ms, Memory: 2052 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.cc
+++ 백준/Gold/21277. 짠돌이 호석/짠돌이 호석.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int n1, m1, n2, m2;
-bool a1[103][103], a2[103][103], temp[103][103];
+bool a1[203][103], a2[53][53], temp[53][53];
 
 bool check(int si, int sj) {
 	for (int di = 0; di < n2; ++di) for (int dj = 0; dj < m2; ++dj) {
@@ -16,7 +16,7 @@ int main() {
 	cin >> n1 >> m1;
 	for (int i = 0; i < n1; ++i) {
 		string s; cin >> s;
-		for (int j = 0; j < m1; ++j) a1[i][j] = s[j]-'0';
+		for (int j = 0; j < m1; ++j) a1[50+i][50+j] = s[j]-'0';
 	}
 
 	cin >> n2 >> m2;
@@ -27,11 +27,12 @@ int main() {
 
 	int ans = 0x7fffffff;
 	for (int turn = 0; turn < 4; ++turn) {
-		for (int si = 0; si <= n1; ++si) for (int sj = 0; sj <= m1; ++sj)
-            if (check(si, sj)) ans = min(ans, max(n1, si+n2) * max(m1, sj+m2));
+		for (int si = 50-n1; si <= 50+n1; ++si) for (int sj = 50-m1; sj <= 50+m1; ++sj)
+            if (check(si, sj)) ans = min(ans, (max(50+n1, si+n2) - min(50, si)) * (max(50+m1, sj+m2) - min(50, sj)));
 		
 		swap(n2, m2);
 		for (int i = 0; i < n2; ++i) for (int j = 0; j < m2; ++j) temp[i][j] = a2[m2-1-j][i];
+		memset(a2, 0, sizeof(a2));
 		for (int i = 0; i < n2; ++i) for (int j = 0; j < m2; ++j) a2[i][j] = temp[i][j];
 	} cout << ans;
 }
