1ea4b79400dab2407ef0bc3d2e211ae9cb830dbb
정수현 <jsh00325@naver.com>
2024-05-14 00:03:29 +0900

    [Gold I] Title: 미네랄, Time: 36 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/2933. 미네랄/미네랄.cc
+++ 백준/Gold/2933. 미네랄/미네랄.cc
@@ -1,111 +1,123 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <utility>
+#include <bits/stdc++.h>
 using namespace std;
-
-typedef pair<int,int> pii;
 #define X first
 #define Y second
+typedef pair<int, int> pii;
 
-int r, c, map[103][103], n, h;
-int di[4] = {0, 1, 0, -1}, dj[4] = {1, 0, -1, 0};
-bool l2r = true, visit[103][103];
-queue<pii> q;
+int r, c;
+string arr[100];
+bool vst[100][100];
+const int di[4] = {0, -1, 0, 1}, dj[4] = {-1, 0, 1, 0};
 
-bool cutBlock(int h) {
-	int sj = 0, d = 1;
-	if (!l2r) {
-		sj = c - 1;
-		d = -1;
-	}
-
-	for (int j = sj; 0 <= j && j < c; j += d)
-		if (map[h][j] == 1) {
-			map[h][j] = 0;
-			return true;
-		}
-	return false;
+inline bool oob(int i, int j) {
+    return (i < 0 || j < 0 || i >= r || j >= c);
 }
-void search() {
-	for (int i = 0; i < r; i++) fill(visit[i], visit[i]+c, false);
-	
-	for (int j = 0; j < c; j++) {
-		if (map[r-1][j] == 1) {
-			q.push({r-1, j}); visit[r-1][j] = true;
-		}
-	}
 
-	while (!q.empty()) {
-		pii cur = q.front(); q.pop();
+// h: 창이 날라온 높이, ori: 창이 날라오는 방향 (0: l->r, 1: r->l)
+pii throwStick(int h, int ori) {
+    int i = h, sj = (ori ? c-1 : 0), d = (ori ? -1 : 1);
 
-		for (int dir = 0; dir < 4; dir++) {
-			pii nxt = {cur.X + di[dir], cur.Y + dj[dir]};
-			if (nxt.X < 0 || nxt.X >= r) continue;
-			if (nxt.Y < 0 || nxt.Y >= c) continue;
-			if (map[nxt.X][nxt.Y] == 0) continue;
-			if (visit[nxt.X][nxt.Y]) continue;
-
-			visit[nxt.X][nxt.Y] = true;
-			q.push(nxt);
-		}
-	}
+    for (int j = sj; 0 <= j && j < c; j += d) {
+        if (arr[i][j] == 'x') {
+            arr[i][j] = '.';
+            return {i, j};
+        }
+    } return {-1, -1};
 }
-int canMove() {
-	int ans = 999;
-	for (int j = 0; j < c; j++) {
-		for (int i = r-1; i >= 0; i--) {
-			if (map[i][j] == 0) continue;
-			if (visit[i][j]) continue;
 
-			int d = 1;
-			while (i+d < r && map[i+d][j] == 0) d++;
+// (i, j)가 포함된 클러스터가 얼마나 떨어질 수 있는지 확인
+int calcDropHeight(int i, int j) {
+    int drop_height = 0x7fffffff;
 
-			ans = min(ans, d-1);
-			break;
-		}
-	}
-	return ans;
+    // 열 단위로 떨어질 높이 계산
+    for (int j = 0; j < c; ++j) {
+        // 클러스터의 아래 블럭 찾기
+        int bottom_i = -1;
+        for (int i = 0; i < r; ++i) if (vst[i][j]) bottom_i = i;
+        if (bottom_i == -1) continue;
+
+        // 떨어질 높이 계산
+        int cur_drop_height = r - bottom_i - 1;
+        for (int delta = 1; bottom_i + delta < r; ++delta) {
+            if (arr[bottom_i + delta][j] == '.') continue;
+            cur_drop_height = delta - 1;
+            break;
+        }
+
+        drop_height = min(drop_height, cur_drop_height);
+    }
+    return drop_height;
 }
-void move(int k) {
-	for (int j = 0; j < c; j++) {
-		for (int i = r-1; i >= 0; i--) {
-			if (map[i][j] == 0) continue;
-			if (visit[i][j]) continue;
 
-			map[i][j] = 0;
-			map[i+k][j] = 1;
-		}
-	}
+void dropCluster(int i, int j) {
+    int drop_height = calcDropHeight(i, j);
+    
+    // 열 단위로 하나씩 이동시킴
+    for (int j = 0; j < c; ++j) for (int i = r-1; i >= 0; --i) {
+        if (!vst[i][j]) continue;   // droppableCluster가 아닌 경우
+
+        arr[i][j] = '.';
+        arr[i + drop_height][j] = 'x';
+    }
 }
-void print() {
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			if (map[i][j]) cout << 'x';
-			else cout << '.';
-		}
-		cout << '\n';
-	}
+
+// 떨어질 수 있는 클러스터인지 확인
+//  -> 가장 낮은 블럭이 땅이 아니면 droppable
+bool judgeDroppable(int i, int j) {
+    memset(vst, false, sizeof(vst));
+    int max_i = -1;
+
+    queue<pii> q;
+    q.push({i, j}); vst[i][j] = true;
+    while (!q.empty()) {
+        pii cur = q.front(); q.pop();
+        max_i = max(max_i, cur.X);
+
+        for (int dir = 0; dir < 4; ++dir) {
+            int ni = cur.X + di[dir], nj = cur.Y + dj[dir];
+            if (oob(ni, nj) || arr[ni][nj] == '.' || vst[ni][nj]) continue;
+
+            q.push({ni, nj}); vst[ni][nj] = true;
+        }
+    }
+
+    return (max_i != r-1);
+}
+
+// 부서진 블럭 근처에서 떨어질 수 있는 클러스터를 찾음
+// 만약 있다면, 그 클러스터를 떨어뜨림
+void searchDroppableCluster(pii broken_block) {
+    for (int d = 0; d < 4; ++d) {
+        int i = broken_block.X + di[d], j = broken_block.Y + dj[d];
+        if (oob(i, j) || arr[i][j] == '.') continue;
+
+        if (judgeDroppable(i, j)) {
+            dropCluster(i, j);
+            return;
+        }
+    }
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+	cin.tie(0)->sync_with_stdio(0);
 
-	cin >> r >> c;
-	for (int i = 0; i < r; i++) {
-		string str; cin >> str;
-		for (int j = 0; j < c; j++)
-			if (str.at(j) == 'x') map[i][j] = 1;
-	}
-	cin >> n;
-	while (n--) {
-		cin >> h;
-		cutBlock(r - h);
-		search();
-		move(canMove());
-		l2r = !l2r;
-	}
-	print();
+    cin >> r >> c;
+    for (int i = 0; i < r; ++i) cin >> arr[i];
+
+    int q; cin >> q;
+    for (int t = 0; t < q; ++t) {
+        int h; cin >> h;
+
+        // arr의 높이에 맞게 변형하여 throw
+        pii broken_block = throwStick(r - h, t & 1);
+        if (broken_block == pii{-1, -1}) continue;
+
+        // 떨어질 수 있는 클러스터를 찾아보기
+        searchDroppableCluster(broken_block);
+    }
+
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j) cout << arr[i][j];
+        cout << '\n';
+    }
 }
