a94ff76f44e3e64b29e0a76cbb4f04642ea41f5f
정수현 <jsh00325@naver.com>
2024-10-10 14:47:16 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.c
+++ 백준/Silver/9012. 괄호/괄호.c
@@ -1,29 +1,22 @@
+#include <stdio.h>
+
+int solve() {
+    char targetString[60];
+    scanf("%s", targetString);
+
+    int topIndex = 0;
+    for (int i = 0; targetString[i] != '\0'; ++i) {
+        if (targetString[i] == '(') ++topIndex;
+        else if (topIndex == 0) return 0;
+        else --topIndex;
+    }
+    return topIndex == 0;
+}
+
 int main() {
-	int n; scanf("%d", &n);
-	char str[51];
-	for (int x = 0; x < n; x++) {
-		int check[51] = { 0, };
-		int cnt = 1;
-		scanf("%s", str);
-		for (int i = 0; i < 50; i++) {
-			if (str[i] == '\0') break;
-			else if (str[i] == '(') check[cnt++]++;
-			else if (str[i] == ')') check[--cnt]--;
-			if (check[0] == -1) {
-				cnt = -1; break;
-			}
-		}
-		if (cnt == -1) printf("NO\n");
-		else {
-			for (int i = 1; i <= 50; i++) {
-				if (check[i] == 0) continue;
-				else {
-					printf("NO\n");
-					cnt = -1;
-					break;
-				}
-			}
-			if (cnt != -1) printf("YES\n");
-		}
-	}
+    int T;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; ++i)
+        printf("%s\n", solve() ? "YES" : "NO");
 }
